5ce4f10addf4dcd2b0aad0a00b0078b26be9b486
Vini <vinicius.gcd@gmail.com>
2024-11-19 14:31:17 -0300

    .

--- Aulas/Modulo 1/Exercicios/d32.py
+++ Aulas/Modulo 1/Exercicios/d32.py
@@ -1,7 +1,9 @@
 #Faça um programa que calcule se o ano é bisexto ou não
-ano = int(input('Digite o ano para verificar se é bisexto? '))
-
+from datetime import date
+ano = int(input('Digite o ano para verificar se é bissexto? Digite 0 para o ano atual '))
+if ano == 0:
+    ano = date.today().year
 if (ano % 4 == 0 and ano % 100 !=0) or (ano % 400 == 0):
-    print ('O ano é bisexto!')
+    print ('O ano de {} é bissexto!'.format(ano))
 else:
-    print ('O ano não é bisexto')
+    print ('O ano {} não é bissexto'.format(ano))
